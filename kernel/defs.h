0ce369121afe0a84e84b960fa10a6cf9f5fec13a
Philip Nguyen <pnguy058@sledge.cs.ucr.edu>
2020-02-23 02:40:29 -0800

    Added declarations for clone function.

--- kernel/defs.h
+++ kernel/defs.h
@@ -120,6 +120,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+void            clone(void(*fcn)(void*),void *, void *);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
