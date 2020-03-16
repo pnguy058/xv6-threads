e21510081daf52a1cff502c191afa2113f1c1c46
jjone041 <jjone041@ucr.edu>
2020-03-16 11:02:01 -0700

    added lock_t structure
    ticket/turn used for fetch-and-add
    flag used for test-and-set

--- kernel/types.h
+++ kernel/types.h
@@ -2,3 +2,8 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+typedef struct __lock_t {
+  uint ticket;
+  uint turn;
+  uint flag;
+} lock_t;
